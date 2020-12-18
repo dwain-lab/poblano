21cb65fa4dcf160659a10dc9bd93f385ad24714c
DWain <dwain22@gmail.com>
2020-12-17 18:48:18 -0600

    2.4 Played with Spatie Media

--- resources/views/layouts/__includes/__head.blade.php
+++ resources/views/layouts/__includes/__head.blade.php
@@ -5,8 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  {{-- <title>Restaurantly Bootstrap Template - Index</title> --}}
-  <title>Site - @yield('title')</title>
+  <title>Restaurantly Bootstrap Template - Index</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
