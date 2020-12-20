4c533ea3e9f29c13744d97ff5af58fa15f68a3e3
DWain <dwain22@gmail.com>
2020-12-20 05:18:01 -0600

    install breadcrumbs

---
+++ app/Providers/BreadcrumbsProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Tabuna\Breadcrumbs\Breadcrumbs;
+use Tabuna\Breadcrumbs\Trail;
+
+class BreadcrumbsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Breadcrumbs::for('photos.index', fn (Trail $trail) =>
+             $trail->push('Photos', route('home'))
+        );
+    }
+}
